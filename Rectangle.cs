5299d1cddd48093721f3a91bc9e0ed5ebfe0587e
Elizaveta <mtkrelizaveta@gmail.com>
2026-03-04 15:35:01 +0500

    hhf

---
+++ Rectangle.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometryShape
+{
+    public class Rectangle
+    {
+        public double Width
+        {
+            get;set;
+        }
+        public double Height
+        {
+            get; set;
+        }
+
+
+        public double Area(double width,double height)
+        {
+           Width= width;
+            Height= height;
+           double _area = Width * Height;
+           return _area;
+        }
+        public double Perimeter(double width, double height)
+        {
+            Width = width;
+            Height = height;
+            double _perimetr = (Width + Height) *2;
+            return _perimetr;
+        }
+        public string ToString()
+        {
+            return $"Площадь прямоугольника равна {Area} и периметр равен {Perimeter}";
+        }
+
+    }
+}
---
+++ Triangle.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometryShape
+{
+    public class Triangle
+    {
+        public double SideA { get; set; }
+        public double SideB { get; set; }
+        public double SideC { get; set; }
+
+        public double Area()
+        {
+            double poluperimeter = (SideA + SideB + SideC) / 2;
+
+            return Math.Sqrt(poluperimeter * (poluperimeter - SideA) * (poluperimeter - SideB) * (poluperimeter - SideC));
+        }
+
+        public double Perimeter()
+        {
+            return SideA + SideB + SideC;
+        }
+
+        public string ToString()
+        {
+            return $"Пощадь треугольника {Area} периметр треугольника {Perimeter}";
+
+        }
+
+        public string Validation()
+        {
+            if (SideA < SideB + SideC)
+                return "некорректные данные";
+            if (SideB < SideA + SideC)
+                return "некорректные данные";
+            if (SideC < SideB + SideA)
+                return "некорректные данные";
+            if(SideA==0 || SideB==0|| SideC==0)
+                return "некорректные данные";
+
+            return "";
+        }
+
+
+    }
+}
